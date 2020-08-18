52b5469d71cd7adfe4acfaacc53f01943b823011
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-08-18 18:15:55 -0500

    adding action creators

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Field, reduxForm, formValues} from 'redux-form'
+import {connect} from 'react-redux'
+import {createStream} from '../../actions'
 
 class StreamCreate extends React.Component{
 
@@ -26,8 +28,8 @@ class StreamCreate extends React.Component{
         ) 
     }
 
-    onSubmit(formValues){
-        console.log(formValues)
+    onSubmit = formValues => {
+        this.props.createStream(formValues)
     }
 
     render(){        
@@ -53,4 +55,6 @@ const validate = formValues =>{
     return errors
 }
 
-export default reduxForm({ form : 'streamCreate', validate})(StreamCreate)
+const formWrapped = reduxForm({ form : 'streamCreate', validate})(StreamCreate)
+
+export default connect (null, {createStream})(formWrapped)
