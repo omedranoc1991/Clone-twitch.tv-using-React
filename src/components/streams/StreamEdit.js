61bf211703cac89ff971034ff2e5cbdca417c03b
Oscar Medrano Castillo <omedranoc@hotmail.com>
2020-08-20 18:55:17 -0500

    creating the delete stream component

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,8 @@
+import _ from 'lodash'
 import React from 'react'
 import {connect} from 'react-redux'
-import {fetchStreams} from '../../actions'
+import {fetchStreams, editStream} from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component{
 
@@ -8,13 +10,24 @@ class StreamEdit extends React.Component{
         this.props.fetchStreams(this.props.match.params.id)
     }
 
+    onSubmit=(formValues) =>{
+        this.props.editStream(this.props.match.params.id, formValues)
+    }
+
    render(){
 
     if(!this.props.stream){
         return <div>Loading...</div>
     }
-
-    return <div>{this.props.stream.title}</div>
+    return(
+        <div>
+            <h3>Edit a Stream</h3>
+            <StreamForm
+             initialValues={_.pick(this.props.stream, 'title','description')}
+             onSubmit={this.onSubmit}
+            />
+        </div>
+    ) 
    }
 }
 
@@ -22,4 +35,4 @@ const mapStateToProps = (state, ownProps)=>{
     return {stream : state.streams[ownProps.match.params.id]}
 }
 
-export default connect(mapStateToProps,{fetchStreams})(StreamEdit)
+export default connect(mapStateToProps,{fetchStreams, editStream})(StreamEdit)
